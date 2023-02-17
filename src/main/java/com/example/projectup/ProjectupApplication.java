e4fd769e24e3cdff59e9c959455c402f6dda1fdf
saebyeolKim <toquf2611@naver.com>
2023-02-17 17:53:32 +0900

    Initial commit

---
+++ src/main/java/com/example/projectup/ProjectupApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projectup/ProjectupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
